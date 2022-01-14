b2025291cd356eb1b02d0a1524f4748b64e3910f
DavidMilai <davidmilai67@gmail.com>
2022-01-14 11:19:37 +0300

    setting up a local network with accounts

--- test/inbox.text.js
+++ test/inbox.text.js
@@ -1,29 +1,48 @@
 const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
-const web3 = new Web3(ganache.provider);
+const web3 = new Web3(ganache.provider());
 
-class Car {
-  park() {
-    return "stopped";
-  }
-  drive() {
-    return "vroom";
-  }
-}
- let car;
+let accounts;
 
-beforeEach(()=>{
-     car = new Car();
+beforeEach(async()=>{
+    //Get a list of all accounts
+    accounts =await web3.eth.getAccounts(); 
 });
 
-describe("Probox", () => {
-  it("can park", () => {
-    assert.equal(car.park(), "stopped");
-  });
-
-  it("can drive", () => {
-    assert.equal(car.drive(), "vroom");
-  });
-
+describe('Inbox',()=>{
+    it('deploys contracts',()=>{
+        console.log(accounts);
+    });
 });
+
+
+
+
+
+
+
+// class Car {
+//   park() {
+//     return "stopped";
+//   }
+//   drive() {
+//     return "vroom";
+//   }
+// }
+//  let car;
+
+// beforeEach(()=>{
+//      car = new Car();
+// });
+
+// describe("Probox", () => {
+//   it("can park", () => {
+//     assert.equal(car.park(), "stopped");
+//   });
+
+//   it("can drive", () => {
+//     assert.equal(car.drive(), "vroom");
+//   });
+
+// });
