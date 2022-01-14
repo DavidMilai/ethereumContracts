62c15e7a6ea41247f1c69c71da549fc87c1af6ef
DavidMilai <davidmilai67@gmail.com>
2022-01-14 09:32:42 +0300

    mocha multiple tests

--- test/inbox.text.js
+++ test/inbox.text.js
@@ -11,10 +11,19 @@ class Car {
     return "vroom";
   }
 }
+ let car;
+
+beforeEach(()=>{
+     car = new Car();
+});
 
 describe("Probox", () => {
   it("can park", () => {
-    const car = new Car();
     assert.equal(car.park(), "stopped");
   });
+
+  it("can drive", () => {
+    assert.equal(car.drive(), "vroom");
+  });
+
 });
