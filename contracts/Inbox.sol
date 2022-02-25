1c8dc39824a64eed63b99b24806c0377fbbb6bf7
DavidMilai <davidmilai67@gmail.com>
2022-02-25 12:41:21 +0300

    lottery contract

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 contract Inbox {
     string public message;
 
-  constructor(string memory initialMessage)public{
+  constructor(string memory initialMessage){
         message = initialMessage;
     }
 
