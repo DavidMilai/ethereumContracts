38b582ce947588df6dfcc98a91b7d60c7dcaa5c7
DavidMilai <davidmilai67@gmail.com>
2022-02-26 16:54:08 +0300

    adding eth to a transaction

--- contracts/ballot.sol
+++ contracts/ballot.sol
@@ -1,16 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-contract Lottery{
+contract Lottery {
     address public manager;
     address[] public players;
 
-  constructor(){
+    constructor() {
         manager = msg.sender;
     }
 
-    function enter()public payable{
+    function enter() public payable {
+        require(msg.value> .01 ether);
         players.push(msg.sender);
     }
-
 }
