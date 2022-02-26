c75dcd2577991b40f48f5af7b1b1737919f9638e
DavidMilai <davidmilai67@gmail.com>
2022-02-26 16:26:14 +0300

    list of players and entering the lottery

--- contracts/ballot.sol
+++ contracts/ballot.sol
@@ -3,8 +3,14 @@ pragma solidity ^0.8.0;
 
 contract Lottery{
     address public manager;
+    address[] public players;
 
   constructor(){
         manager = msg.sender;
     }
+
+    function enter()public payable{
+        players.push(msg.sender);
+    }
+
 }
