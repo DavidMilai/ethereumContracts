734eec4b83d5cfa6e42d349841e81c1f18d4ce54
DavidMilai <davidmilai67@gmail.com>
2022-02-28 20:21:02 +0300

    generating a random number

--- contracts/ballot.sol
+++ contracts/ballot.sol
@@ -10,8 +10,17 @@ contract Lottery {
     }
 
     function enter() public payable {
-        require(msg.value> .01 ether);
+        require(msg.value > .01 ether);
         // checking to see amount
         players.push(msg.sender);
+    }
+
+    function random() private view returns (uint256) {
+        return
+            uint256(
+                keccak256(
+                    abi.encodePacked(block.difficulty, block.timestamp, players)
+                )
+            );
     }
 }
