5c89ab5ec044c3e64e95dba2c755a419325f3003
DavidMilai <davidmilai67@gmail.com>
2022-03-01 22:26:51 +0300

    Lottery test deployment

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -28,7 +28,7 @@ describe("Inbox", () => {
     const message = await inbox.methods.message().call();
     assert.equal(message, INITIAL_MESSAGE);
   });
- 
+
   it("updates the message", async () => {
     await inbox.methods.setMessage("bye").send({ from: accounts[0] });
     const message = await inbox.methods.message().call();
---
+++ test/lottery.test.js
@@ -0,0 +1,19 @@
+//ts-check
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
+const { abi, bytecode } = require("../compile");
+const web3 = new Web3(ganache.provider());
+
+let lottery;
+let accounts;
+
+beforeEach(async () => {
+  //Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+
+  //using accounts to deploy a contract
+  lottery = await new web3.eth.Contract(abi)
+    .deploy({ data: bytecode })
+    .send({ from: accounts[0], gas: "1000000" });
+});
